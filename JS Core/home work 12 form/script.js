497291b52494eef8f0abd0f1af3f0ffddb961ac2
Volodymyr <88154076+VolodymyrBochuliak@users.noreply.github.com>
2022-09-02 23:43:35 +0300

    update

--- JS Core/home work 12 form/script.js
+++ JS Core/home work 12 form/script.js
@@ -2,7 +2,7 @@
 
 const textRegEx = /^[a-zA-Z]{3,20}$/;
 const mailRegEx = /^([a-zA-Z0-9_\-\.]{1,255})@([a-zA-Z0-9_\-\.]{1,255})\.([a-zA-Z]{2,64})$/;
-const passRegEx = /[^(!@#$%^&*)][\w]{7,14}/;
+const passRegEx = /[^(!@#$%^&*)][\w]{5,12}/;
 
 
 // Getting elements from a page
@@ -13,7 +13,8 @@ const login = document.querySelector('#userLogin');
 const password = document.querySelector('#pass');
 const email = document.querySelector('#mail');
 
-const addUser = document.querySelector('.actionBtn');
+const addUser = document.querySelector('.addBtn');
+const saveUser = document.querySelector('.saveBtn');
 
 
 // Add event listener
@@ -31,14 +32,17 @@ function FildValid (regExp, fild) {
 
 login.addEventListener('blur', () => {
     FildValid(textRegEx, login);
+    validateAddUser();
 });
 
 email.addEventListener('blur', () => {
     FildValid(mailRegEx, email);
+    validateAddUser();
 });
 
 password.addEventListener ('blur', () => {
     FildValid(passRegEx, password);
+    validateAddUser();
 });
 
 
@@ -47,14 +51,12 @@ function validateAddUser() {
     if (
         FildValid(textRegEx, login) &&
         FildValid(mailRegEx, email) &&
-        FildValid(passRegEx, password) == true
+        FildValid(passRegEx, password)
         ){
-
-        console.log('access granted');
+        // console.log('access granted');
         return addUser.disabled = false;
     } else {
-        
-        console.log('access denied');
+        // console.log('access denied');
         return addUser.disabled = true;
     }
 }
@@ -69,10 +71,6 @@ addUser.addEventListener('click', () => {
     validateAddUser();
     createUserItem();
     
-    clearFilds(login);
-    clearFilds(email);
-    clearFilds(password);
-
 });
 
 let listNum = 0;
@@ -109,8 +107,9 @@ function createUserItem() {
         // console.log(value);
 
         const newCell = document.createElement('td');
+        newCell.classList.add(`${key}`);
         newRow.append(newCell);
-        
+                
         newCell.textContent = value;
 
         if (key === "positionNumber") {
@@ -128,13 +127,56 @@ function createUserItem() {
 
     });
 
+    clearFilds(login);
+    clearFilds(email);
+    clearFilds(password);
+    addUser.disabled = true;
+
+    const userLog = document.querySelector('.userLog');
+    const userPass = document.querySelector('.userPass');
+    const userMail = document.querySelector('.userMail');
+
     deleteBtn.addEventListener('click', () => {
         // const deleteRow = newRow.remove();
         newRow.remove();
     });
+    
+    editBtn.addEventListener('click', () => {
+    // повертаєм значення в інпути
+        login.value = userLog.innerText;
+        password.value = userPass.innerText;
+        password.type = 'text';
+        email.value = userMail.innerText;
+        // міняєм кнопку add на save
+        addUser.style.display = 'none';
+        saveUser.style.display = 'block';
+
+        saveUser.addEventListener('click', () => {
+            
+            // переписуєм значення з інпутів в таблицю
+            userLog.innerText = login.value;
+            userPass.innerText = password.value;
+            userMail.innerText = email.value;
+            
+            // чистим поля інпутів
+            clearFilds(login);
+            clearFilds(email);
+            clearFilds(password);
+            
+            addUser.style.display = 'block';
+            saveUser.style.display = 'none';
+        });
+
+
+    });
+   
+    return {userLog, userPass, userMail, editBtn, deleteBtn}
 }
 
+
+
+
 function clearFilds (input) {
-    setTimeout(() => {input.value = '';}, 1000);
+    setTimeout(() => {input.value = '';}, 100);
 }
 
