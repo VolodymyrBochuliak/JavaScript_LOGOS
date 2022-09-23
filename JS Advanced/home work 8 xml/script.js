8d692e4c0016851ee94a73ff29efbc856c3c76ce
Volodymyr <88154076+VolodymyrBochuliak@users.noreply.github.com>
2022-09-23 18:06:20 +0300

    .

--- JS Advanced/home work 8 xml/script.js
+++ JS Advanced/home work 8 xml/script.js
@@ -1,78 +1,57 @@
 
-// class Post {
-//     constructor(title, author) {
-//         this.title = title;
-//         this.author = author;
-//     }
-// }
-
-// const post = new Post('my title', 'Martyn');
+    const more_info = `https://www.omdbapi.com/?apikey=1a09f488&s=${Title}&plot=full`;
 
 async function getData() {
     
-    const name = document.querySelector('#serch_fild').value;
-    // const year = year.value;
-    const url = `https://www.omdbapi.com/?apikey=ab1f95bf&t=${name}&y=${100}`;
-
+    const name = document.querySelector('#first_serch_fild').value;
+    const url_for_serch = `https://www.omdbapi.com/?apikey=1a09f488&s=${name}`;
     try {
-        const response = await fetch(url);
+       
+        const response = await fetch(url_for_serch);
         const data = await response.json();
 
-        console.log(data);
-        console.log('done successfully');
+        console.log(data.Search);
+        let dataArray = data.Search;
+        
+        showMovies(dataArray);
+
     } catch (e) {
         console.log(e);
     }
-
 }
 
-const getBtn = document.querySelector('.btn-get');
-getBtn.addEventListener('click', getData);
+const getSerch = document.querySelector('.btn-get_by_serch');
+getSerch.addEventListener('click', ()=> {
 
+    getData();
 
+});
 
-// async function postData() {
-//     try {
-//         await fetch(getUrl, {
-//             method: 'POST',
-//             body: JSON.stringify(post),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//         return console.log('data post successfully');
-//     } catch (error) {
-//         return console.log(error);
-//     }
-// }
+function showMovies(obj) {
+    const main = document.querySelector('.main_section');
+    main.innerHTML = '';
 
-// async function deleteData() {
-//     try {
-//         await fetch(deleteUrl, {
-//             method: 'DELETE'
-//         });
-//         return console.log('deleted successfully');
-//     } catch (error) {
-//         return console.log(error);
-//     }
-// }
-
-
-// let postsUrl = 'http://localhost:3000/posts';
-// let commentsUrl = 'http://localhost:3000/comments';
-
-
-// const getData = async () => {
-//     const postsResponse = await fetch(postsUrl);
-//     const commentsResponse = await fetch(commentsUrl);
-//     const postsData = await postsResponse.json();
-//     const commentsData = await commentsResponse.json();
-//     const result = await Promise.all([postsData, commentsData]);
-//     console.log('result =>', result);
-// }
-
-// document.querySelector('.btn-get').addEventListener('click', getData);
-
-
-
+   obj.forEach( movie => {
+        
+        const {Poster, Title, Year, imdbID, overview, id} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${Poster}" alt="${Title}">
+            <div class="movie-info">
+                <h3>${Title}</h3>
+                <span class="yers">Year of release: ${Year}</span> <br>
+                <span class="yers">IMDb ID: ${imdbID}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+                <br/> 
+                <button class="know_more" id="${id}">More...</button
+            </div>
+        
+        `;
+        main.appendChild(movieEl);
+    });
+}
 
