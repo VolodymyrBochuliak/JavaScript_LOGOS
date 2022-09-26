17dac8803da279f7269a029b6a9ec1b7edfd0fb7
Volodymyr <88154076+VolodymyrBochuliak@users.noreply.github.com>
2022-09-26 19:02:35 +0300

    .

--- JS Advanced/home work 9 TS/script.js
+++ JS Advanced/home work 9 TS/script.js
@@ -1,1 +1,17 @@
-var ts = "hello";
+////////////////////////////
+var city;
+city = 'Kyiv';
+city = 'Lviv';
+var address = city;
+console.log(address);
+///////////////////////////
+var user_input = prompt('tape number');
+if (user_input) {
+    console.log('even num');
+}
+else if (user_input) {
+    console.log('odd num');
+}
+else {
+    console.log('num equal null');
+}
--- JS Advanced/home work 9 TS/script.ts
+++ JS Advanced/home work 9 TS/script.ts
@@ -1,1 +1,17 @@
-const ts :string = "hello";
+////////////////////////////
+let city :string;
+city = 'Kyiv';
+city = 'Lviv';
+let address :string = city;
+console.log(address);
+///////////////////////////
+
+let user_input = prompt('tape number');
+
+if (user_input ) {
+    console.log('even num');
+} else  if ( user_input) {
+    console.log('odd num');
+} else {
+    console.log('num equal null');
+}
