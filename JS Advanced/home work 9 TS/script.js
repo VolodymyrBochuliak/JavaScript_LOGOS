23373e887745c5fa968ac7564f0655b5368d99cb
Volodymyr <88154076+VolodymyrBochuliak@users.noreply.github.com>
2022-09-28 00:53:33 +0300

    .

--- JS Advanced/home work 9 TS/script.js
+++ JS Advanced/home work 9 TS/script.js
@@ -1,17 +1,29 @@
-////////////////////////////
-var city;
+let city;
 city = 'Kyiv';
 city = 'Lviv';
-var address = city;
+let address = city;
 console.log(address);
-///////////////////////////
-var user_input = prompt('tape number');
-if (user_input) {
+let input = prompt('tape number');
+let user_input = +input;
+if (user_input % 2 === 0) {
     console.log('even num');
 }
-else if (user_input) {
-    console.log('odd num');
-}
-else {
+else if (user_input === 0) {
     console.log('num equal null');
 }
+else {
+    console.log('odd num');
+}
+function maxArrValue(...args) {
+    if (args.length < 1) {
+        alert('Max of empty list');
+    }
+    let m = args[0];
+    args.forEach(number => {
+        if (number > m) {
+            m = number;
+        }
+    });
+    return m;
+}
+console.log(maxArrValue(-8, 5, 30, 8, 1, 10));
--- JS Advanced/home work 9 TS/script.ts
+++ JS Advanced/home work 9 TS/script.ts
@@ -6,12 +6,34 @@ let address :string = city;
 console.log(address);
 ///////////////////////////
 
-let user_input = prompt('tape number');
+let input = prompt('tape number');
+let user_input :number = + input;
 
-if (user_input ) {
+if (user_input % 2 === 0 ) {
     console.log('even num');
-} else  if ( user_input) {
-    console.log('odd num');
-} else {
+    // console.log(typeof user_input );
+} else  if ( user_input === 0) {
     console.log('num equal null');
+} else {
+    console.log('odd num');
 }
+
+//////////////////////////////////////////
+function maxArrValue (...args: Array<number>) : number | string {
+    if (args.length < 1){ 
+        alert ('Max of empty list'); 
+    }
+   let m: any = args[0];
+    args.forEach(number => {
+        if (number > m) {
+            m = number
+        }
+    });
+    return m;
+}
+console.log(maxArrValue(-8,5,30,8,1,10));
+//////////////////////////////////////////
+
+//////////////////////////////////////////
+
+//////////////////////////////////////////
